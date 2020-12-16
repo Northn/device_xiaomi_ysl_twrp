789a58402b18b207602cf6a754618db9e60a31ca
immat0x1 <fl3ksmashina@gmail.com>
2020-12-16 21:58:27 +0500

    ysl: Inherit some packages

--- omni_ysl.mk
+++ omni_ysl.mk
@@ -12,15 +12,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Release name
-PRODUCT_RELEASE_NAME := ysl
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
@@ -28,3 +35,4 @@ PRODUCT_NAME := omni_ysl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := ysl
